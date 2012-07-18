6f2e455a77179f81dfd19037c078c158d3e14ee2
Valerio Virgillito <valerio@motorola.com>
2012-07-18 16:00:01 -0700

    fixed highlight of file icons and grid menu
    Signed-off-by: Valerio Virgillito <valerio@motorola.com>

--- js/ui/menu/menu-controller.js
+++ js/ui/menu/menu-controller.js
@@ -151,7 +151,7 @@ exports.MenuController = Montage.create(Component, {
     handlegrid: {
         value: function(value) {
             this.topLevelMenu[2].entries[5].checked = !this.topLevelMenu[2].entries[5].checked;
-            document.application.model.show3dGrid = this.topLevelMenu[2].entries[4].checked;
+            document.application.model.show3dGrid = this.topLevelMenu[2].entries[5].checked;
         }
     },
 
